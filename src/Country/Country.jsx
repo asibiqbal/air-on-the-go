9e7ea421c2d284c72923f638776a40edf0a4108c
asibiqbal <mohammadasibiqbal@gmail.com>
2026-06-03 19:26:53 +0600

    Added css styles and visited, not visited button

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
-    console.log(country.name)
+    const [visited , setVisited] = useState(false)
+
+    const getVisited = () => {
+        setVisited(!visited)
+    }
     return (
-        <div className="country-container countries-grid">
+        
+        <div className={`country-container ${(visited) ? 'country-visited' : 'country-not-visited'}`}>
             <img className="country-img" src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2 className="country-name">Country Name : {country.name.common}</h2>
             <p className="country-official">Official Name : {country.name.official}</p>
             <p className="country-capital">Capital : {country.capital.capital}</p>
             <p className="country-population">Population : {country.population.population}</p>
             <p className="country-region">Region : {country.region.region}</p>
+            <button onClick={getVisited}>{
+                (visited) ? 'Visited' : 'Not Visited'
+                }</button>
         </div>
     );
 };
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,15 +1,17 @@
 import { use } from "react";
 import Country from "../../Country/Country";
-
+import './Countries.css';
 const Countries = ({getCountries}) => {
     const allCountries = use(getCountries);
     const getAllCountries = allCountries.countries
     return (
         <div>
             <h1>Total Countries : {getAllCountries.length}</h1>
+            <div className="country-wrapper">
             {
-                getAllCountries.map(country => <Country country={country}></Country>)
+                getAllCountries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
